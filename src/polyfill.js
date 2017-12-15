f11d06e74eb54caae39e7b78eb942d0ea59878be
Jan Miksovsky <jan@quickui.org>
2017-12-15 13:52:30 -0800

    createElement + array example

--- src/polyfill.js
+++ src/polyfill.js
@@ -2,13 +2,36 @@ import applyProperties from './applyProperties.js';
 import applyPropertiesById from './applyPropertiesById.js';
 
 
+const original = {
+  createElement: document.createElement,
+  createElementNS: document.createElementNS
+};
+
+
 Element.prototype.applyProperties = function(props) {
   return applyProperties(this, props);
 };
 
-Document.prototype.applyPropertiesById = function(props) {
-  return applyPropertiesById(this, props);
-};
+Object.assign(document, {
+
+  applyPropertiesById: function(props) {
+    return applyPropertiesById(this, props);
+  },
+
+  createElement: function(tagName, props) {
+    const element = original.createElement.call(this, tagName);
+    element.applyProperties(props);
+    return element;
+  },
+
+  createElementNS: function(tagName, props) {
+    const element = original.createElementNS.call(this, tagName);
+    element.applyProperties(props);
+    return element;
+  }
+
+});
+
 DocumentFragment.prototype.applyPropertiesById = function(props) {
   return applyPropertiesById(this, props);
 };
